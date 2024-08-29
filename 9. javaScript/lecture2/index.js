714f9ef075d5dc5e96bb35becb89f5ad04c003eb
Bhautik dudhat <bdudhat293@gmail.com>
2024-08-29 18:10:21 +0530

    29-08-24

---
+++ 9. javaScript/lecture2/index.js
@@ -0,0 +1,29 @@
+
+    // console.log("hello world");
+    // console.log(2 + 2);
+    // console.log(2 + "4");
+    // console.log(4 + "2" - 3);
+    //  console.warn("Hello world !")
+     // console.warn("hello world");  - show warning 
+     // alert("hello world!!");
+
+
+
+    // document.getElementById("box").style.color = "red"
+
+    // JavaScript DataTypes 
+
+    // 1. Primitive DataTypes  - Immutable 
+    
+    // Number 
+    // String 
+    // Boolean
+    // BigInt 
+    // undefined 
+    // null 
+    // Symbol 
+
+    // 2. Non - Primitive DataTypes  - Mutable 
+ 
+    // Array 
+    // Object 
