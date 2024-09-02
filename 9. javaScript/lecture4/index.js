dc6ca6179fa70b17ef132a85da83569cfe48effc
Bhautik dudhat <bdudhat293@gmail.com>
2024-09-02 17:53:05 +0530

    02-09-24

---
+++ 9. javaScript/lecture4/index.js
@@ -0,0 +1,110 @@
+//  document.getElementById("demo").innerHTML = "Hello World!"
+
+//  Case-Sensitive Language --------------
+
+
+
+// var name = "raju";
+// var Name = "ramesh";
+// var NAME = "Rahul";
+
+// console.log(name);
+// console.log(Name);
+// console.log(NAME);
+
+
+
+//  Cases in JavaScript ------------------
+
+
+//   snake case 
+
+// {
+//     let number_of = 35;
+//     console.log(number_of)
+// }
+
+// Kabab Case 
+
+// {
+//     let item-1 = 35;
+//     console.log(item-1)
+// }
+
+// Camel Case 
+
+// {
+//     let numberDonut = "hello"
+//     console.log(numberDonut)
+// }
+
+// pascal Case 
+
+// {
+//     let NumberDonut = "hello"
+//     console.log(NumberDonut)
+// }
+
+
+// Template literals ---------------------
+
+//  let number1 = 'hello - world';
+//  let number2 = "hello-world";
+//  let number3 = `hello-world`
+
+//  console.log(number1);
+//  console.log(number2);
+//  console.log(number3);
+
+//  console.log(number2.length);
+//  console.log(number1.length);
+//  console.log(number3.length);
+
+// ---------not valid
+// let num1 = 'Hello 'javascript'';
+// let num2 = "Hello "Javscript"";
+// let num3 = `Hello `Javscript``
+
+
+// ----------valid 
+// let num4 = "Hello 'javascript' is";
+// console.log(num4);
+
+// let num5 = 'hello "javascript" is ';
+// console.log(num5)
+
+// let num6 = `This is "javascript" web`;
+// console.log(num6)
+
+// let num7 = 'this is `javscript`';
+// console.log(num7);
+
+// let num8 = "this is `javascript` is ";
+// console.log(num8);
+
+
+{
+    let name = "javascipt";
+
+    // console.log("i am learn 'name' today")
+
+    // - only in backticks
+    // console.log(`i am learn ${name} today`);    
+    
+}
+
+{
+    //  - new line and space in '' quote and "" double quote 
+    // let str = "this is strings \t \t \n in javscript";
+    // console.log(str);
+    
+    
+    let str1 = `this is
+     javscript
+      !!`
+    console.log(str1);
+    
+}
+
+
+ 
