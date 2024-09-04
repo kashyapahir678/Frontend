80828062c811903150d7edab00e0597986ea6b3d
Bhautik dudhat <bdudhat293@gmail.com>
2024-09-04 17:29:54 +0530

    04-09-24

---
+++ 9. javaScript/lecture5/style.js
@@ -0,0 +1,149 @@
+
+// javascript Operator 
+
+// Arithmetic operator 
+// Assignment operator 
+// comperison operator 
+// Conditional Operator 
+// Logical Operator 
+// Unary operator 
+// Binary operator 
+// Ternary Operator 
+// String Operator 
+
+
+// Arithmetic Operator 
+
+// {
+//     let x = 5;
+//     let y = 2;
+//     console.log(x + y);
+//     console.log(x - y);
+//     console.log(x * y);
+//     console.log(x / y);
+//     console.log(x % y);
+//     console.log(x ** y);
+    
+// }
+
+// Asignment operator 
+
+// {
+//     let a = 5;
+//     let b = 4;
+//     let c;
+
+//     // console.log(c = a+b);
+
+//     // = a - b 
+//     console.log(a);
+//     console.log(c = a-=b);
+
+//     // = a * b 
+//     console.log(a);
+//     console.log(c = a*=b);
+
+//     // = a / b  
+//     console.log(a);
+//     console.log(c = a/=b);
+
+//     // = a % b 
+//     console.log(a);
+//     console.log(c = a%=b);
+
+//     // = a ** b 
+//     console.log(a);
+//     console.log(c = a**=b);
+    
+// }
+
+
+//Comperison Operator 
+
+{
+    // ==
+    // {
+    //     let a = 'hello'
+    //     let b = 'world'
+    //     console.log(a == b);   
+    // }
+    // {
+    //     let a = 10
+    //     let b = '10'
+    //     console.log(a == b);   
+    // }
+    // {
+    //     let a = 'hello'
+    //     let b = 'hello'
+    //     console.log(a == b);   
+    // }
+
+    // !=
+    // {
+    //     let a = 'hello'
+    //     let b = 'world'
+    //     console.log(a != b);   
+    // }
+    // {
+    //     let a = 10
+    //     let b = '10'
+    //     console.log(a != b);   
+    // }
+    // {
+    //     let a = 'hello'
+    //     let b = 'hello'
+    //     console.log(a != b);   
+    // }   
+
+    // ===
+
+    // {
+    //     let a = 'hello'
+    //     let b = 'world'
+    //     console.log(a === b);   
+    // }
+    // {
+    //     let a = 10
+    //     let b = '10'
+    //     console.log(a === b);   
+    // }
+    // {
+    //     let a = 'hello'
+    //     let b = 'hello'
+    //     console.log(a === b);   
+    // }   
+
+    // !== 
+
+    // {
+    //     let a = 'hello'
+    //     let b = 'world'
+    //     console.log(a !== b);   
+    // }
+    // {
+    //     let a = 10
+    //     let b = '10'
+    //     console.log(a !== b);   
+    // }
+
+    // < 
+    // let num1 = 15;
+    // let num2 = 20;
+    // console.log(num1 < num2);
+    
+    // >
+    // let num1 = 15;
+    // let num2 = 20;
+    // console.log(num1 > num2);
+
+    // <=
+    // let num1 = 15;
+    // let num2 = 20;
+    // console.log(num1 <= num2);
+    
+    // >=
+    // let num1 = 15;
+    // let num2 = 20;
+    // console.log(num1 >= num2);
+}
+
---
+++ 9. javaScript/lecture6/index.js
@@ -0,0 +1,90 @@
+
+// Logical operator 
+
+// AND operator  - &&
+
+// {
+//     let a = true;
+//     let b = false;
+//     let c = true;
+
+//     console.log(a && b);
+//     console.log(a && c);
+//     console.log(b && b);
+    
+// }
+
+// OR operator  - ||
+
+{
+    //     let a = true;
+    //     let b = false;
+    //     let c = true;
+    
+    //     console.log(a || b);
+    //     console.log(a || c);
+    //     console.log(b || b);
+        
+ }
+
+ // NOT operator  - !
+
+{
+    //     let a = true;
+    //     let b = false;
+    //     let c = true;
+    
+    //     console.log(!(a || b));
+    //     console.log(!(a || c));
+    //     console.log(!(b || b));
+        
+ }
+    
+
+ // Unary Operator 
+
+//  const x = "1";
+//  const y = 1;
+
+//  console.log(+x)
+//  console.log(+y)
+//  console.log(+"")
+//  console.log(+true)
+//  console.log(+false)
+//  console.log(+"hello")
+
+{
+    // let x = 10 + 10 + 10 + "10" + 10 + 10 + 10 -20;
+
+    // output :-  
+    // 3010101010 - 20
+    // 3010100990
+
+    // console.log(x);
+}
+
+
+// string Operator 
+
+// {
+//     let str = "skill" + "qode";
+//     console.log(str);
+// }
+
+//Turnary Operator  - ? :
+
+// {
+//     let x = "hello";
+//     let y = "world";
+
+//     console.log(false ? x : y)
+
+// }
+
+
+//Bitwise Operator 
+
+// 1. Bitwise AND operator  ( & )
+// 1. Bitwise OR operator  ( | )
+// 1. Bitwise XOR operator  ( ^ )
+// 1. Bitwise AND operator  ( ~ )
