7383da0de1dfe50369b5b0e140f095d0e5013ff9
Bhautik dudhat <bdudhat293@gmail.com>
2024-09-09 17:52:20 +0530

    09-09-2024

---
+++ 9. javaScript/Program1/index.js
@@ -0,0 +1,32 @@
+
+// 1. Add two number input by the user 
+
+// const num1 = parseInt(prompt("enter the first number"));
+
+// const num2 = parseInt(prompt("enter the second number"));
+
+// const sum = num1 + num2;
+
+// console.log(`the sum is ${sum}`)
+
+
+
+
+// 2. 2.Calculate the area of a rectangle
+
+// let width = 5;
+// let height = 10;
+
+// const area = width * height;
+
+// console.log(`The area of rectangle is ${area}`)
+
+
+
+// 3. Convert temperature from Celsius to Fahrenheit
+
+// const n = prompt("enter the value ");
+
+// const CelToFahr = (n*9 / 5 + 32);
+
+// console.log(CelToFahr);
---
+++ 9. javaScript/lecture8/index.js
@@ -0,0 +1,58 @@
+
+
+// For of Loop in javascript 
+
+// let array = [10]
+// if value is single and integer so print is value 
+// let array1 = new Array(20); 
+
+// console.log(array)
+// console.log(array1.length)
+
+// console.log(typeof array)
+// console.log(typeof array1);
+
+// let newArray = ['kiwi', 'apple', 'mango','orange','graps'];
+
+// console.log(newArray);
+
+// --------------access ARRAY 
+// console.log(newArray[0])
+// console.log(newArray['1'])
+
+// console.log(newArray[0])
+// console.log(newArray[1])
+// console.log(newArray[2])
+// console.log(newArray[3])
+// console.log(newArray[4])
+
+// console.log(newArray.length)
+
+// for(let value of newArray){
+//     console.log(value)
+// }
+
+
+
+
+// For in loop in javascript 
+
+let obj = {1: '11', 2: "22"}
+let obj2 = new Object({1: "111", 2: '222'})
+
+// console.log(obj)
+// console.log(obj2)
+
+// console.log(obj === obj);   - false
+// console.log(array === array);   - false
+// console.log(str === str1);   - true
+
+// ---------access OBJECT 
+// console.log(obj2[1])
+// console.log(obj[2])
+// console.log(obj2['2'])
+// console.log(obj['1'])
+
+// for(let key in obj){
+//     console.log(`${key} = ${obj[key]}`)
+// }
