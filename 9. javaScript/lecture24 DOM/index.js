9d224fd64774d19f23fab5ce4fff802ad078db6b
KashyapAhir <kashyapahir786@gmail.com>
2024-10-10 17:53:33 +0530

    10-10-24

--- 9. javaScript/lecture24 DOM/index.js
+++ 9. javaScript/lecture24 DOM/index.js
@@ -17,3 +17,59 @@ let dom1 = document.getElementById('firstHeading').className
 console.log(dom1);
 let dom2 = document.getElementById('firstHeading').classList[1]
 console.log(dom2);  
+
+
+
+/* Javascript Document Object Model Method [ DOM ] */
+
+// document.getElementById()
+
+// {
+//     document.getElementById('heading').innerHTML = "Hello , Javascript DOM"
+// }
+
+// {
+//     const changeContent = () => {
+//         console.log( document.getElementById('heading').innerHTML = "Hello , Javascript DOM");
+
+//     }
+
+//     console.log(changeContent());
+
+// }
+
+// {
+//   function changeContent() {
+//     document.getElementById("heading").innerHTML = "Hello , Javascript DOM";
+//     console.log(changeContent);
+//     document.getElementById("heading").style.backgroundColor = "purple";
+//     document.getElementById("heading").style.color = "white";
+//   }
+
+//   function hideContent() {
+//     document.getElementById("heading").style.display = "none";
+//   }
+
+//   function showContent() {
+//     document.getElementById("heading").style.display = "block";
+//   }
+// }
+
+
+
+// console.log(document.getElementById('heading').id)
+
+// console.log(document.getElementsByTagName('h1'))
+// console.log(document.getElementsByTagName('h1')[1])
+// console.log(document.getElementsByTagName('h1')[0])
+
+// console.log(document.getElementsByTagName('h1')[1].style.backgroundColor = "red");
+
+// console.log(document.getElementsByClassName('box')[0]);
+// console.log(document.getElementsByClassName('box')[1]);
+
+// console.log(document.getElementsByClassName('box')[1].style.color = "white");
+
+// console.log(document.getElementsByName('heading Text'));
+
+// console.log(document.getElementsByTagNameNS('http://www.w3.org/1999/xhtml' , 'h1')[3]); 
---
+++ 9. javaScript/lecture25 DOM/index.js
@@ -0,0 +1,54 @@
+/* Javascript Document Object Model Method [ DOM ] */
+
+/* javascript DOM innerHTML , innerText , textContent */
+
+/* New Content / Element Javascript */
+
+// {
+
+//     let newElement = document.createElement('h1')
+//     newElement.innerHTML = "This is Heading Tag"
+//     newElement.setAttribute('id' , 'firstHeading')
+//     newElement.setAttribute('class' , 'heading')
+//     document.body.appendChild(newElement)
+
+//     console.log(newElement);
+    
+// }
+
+
+// {
+//    let DOM1 =  document.getElementById('firstHeading').innerHTML
+
+//    let DOM2 = document.getElementById('firstHeading').innerText
+
+//    let DOM3 = document.getElementById('firstHeading').textContent
+
+//     console.log(DOM1);
+
+//     console.log(DOM2);
+
+//     console.log(DOM3);   
+// }
+
+// {
+//     function elementStyle1(){
+//         let dom = document.getElementById('firstHeading')
+//         dom.classList.add('heading')
+//     }
+//     function elementStyle2(){
+//         let dom = document.getElementById('firstHeading')
+//         dom.classList.remove('heading')
+//     }
+//     function elementStyle3(){
+//         let dom = document.getElementById('firstHeading')
+//         dom.classList.toggle('heading')
+//     }
+// }
+
+// {
+//     let dom = document.getElementById('firstHeading').addEventListener('dblclick' , () => {
+//         let dom = document.getElementById('firstHeading')
+//         dom.classList.add('heading')
+//     } )
+// }
