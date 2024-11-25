bbdbc77617d836e30b1570620b2a729f8cf90ef5
kashyapahir <kashyapahir786@gmail.com>
2024-11-25 17:44:12 +0530

    25-11-24

---
+++ 11. react/lecture_4/jsx/src/components/Todo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <>
+            <h1>Hedy Lamarrs Todos</h1>
+            <img
+                src="https://i.imgur.com/yXOvdOSs.jpg"
+                alt="Hedy Lamarr"
+                className="photo"
+            />
+            <ul>
+                ...
+            </ul>
+        </>
+  )
+}
+
+
+export default Todo
