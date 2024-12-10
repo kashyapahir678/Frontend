c942c7c0e546c47cf5f0cb65651972f14174e720
kashyapahir <kashyapahir786@gmail.com>
2024-12-10 17:56:30 +0530

    10-12-2024

--- 11. react/lecture_9/state/src/components/State.jsx
+++ 11. react/lecture_9/state/src/components/State.jsx
@@ -4,23 +4,38 @@ import { AnimalList } from './Data'
 const State = () => {
 
     const [index, setIndex] = useState(0);
+    const [input, setInput] = useState(index)
+    const [show,setShow] = useState(false)
 
     const handleNext = () => {
-        // if(index == ){
-        //     setIndex(index)
-        // }
-        // else{
-        //     setIndex(index + 1)
-        // }
+        if (index == 9) {
+            setIndex(0)
+        }
+        else {
+            setIndex(index + 1);
+        }
     }
 
     const handlePrev = () => {
-        if(index > 0){
+        if (index > 0) {
             setIndex(index - 1)
         }
-        else{
+        else {
             setIndex(index)
         }
+    }
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handlesubmit = () => {
+        setIndex(input - 1)
+
+    }
+
+    const handleShow = () => {
+        setShow(true)
     }
 
     const listCard = [AnimalList[index]];
@@ -28,16 +43,14 @@ const State = () => {
     return (
         <div>
 
-            <h1>Card Number is {index}</h1>
-            <button onClick={handlePrev}>Previous</button>
-            <button onClick={handleNext}>Next</button>
+            {/* <h1 className='text-center m-3'>{index} Card</h1> */}
 
-            <div className='flex flex-wrap justify-around gap-y-2'>
+            <div className='flex flex-wrap justify-around gap-y-2 my-5'>
 
 
                 {listCard.map((item) => {
                     return (
-                        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div key={item.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                             <a href="#">
                                 <img className="rounded-t-lg h-52 w-full object-cover" src={item.image} alt="" />
                             </a>
@@ -45,15 +58,32 @@ const State = () => {
                                 <a href="#">
                                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.name}</h5>
                                 </a>
+
+                                {show && 
                                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.description}</p>
-                                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                }
+
+                                {!show ?
+                                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleShow}>
                                     Read more
                                 </a>
+                                :
+                                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => setShow(false)}>
+                                    less more
+                                </a>}
                             </div>
                         </div>
                     )
                 })}
 
+            </div>
+            <div className='flex text-center justify-evenly my-4'>
+                <button className='py-2 px-4 text-white bg-black cursor-pointer font-bold rounded-md' onClick={handlePrev}>Previous</button>
+                <div className='flex text-black  font-bold rounded-md py-2 px-4 border-2'>
+                    <input className='px-4 py2 border-top ' type="number" placeholder='Enter the number' onChange={handleChange} value={input} />
+                    <button className='bg-black cursor-pointer text-white py-2 px-4' onClick={handlesubmit}>Click</button>
+                </div>
+                <button className='py-2 px-4 text-white outline-dotted bg-black cursor-pointer font-bold rounded-md' onClick={handleNext}>Next</button>
             </div>
         </div>
     )
